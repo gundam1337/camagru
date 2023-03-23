df3eb3d556e415c74492975419b8e3a43586166c
gundam1337 <omarderkaoui1337@gmail.com>
2023-03-23 01:38:55 +0000

    working on the reset the password future

--- CssAndHtml/new_password.php
+++ CssAndHtml/new_password.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CssAndHtml/index.css">
     <link rel="stylesheet" href="../CssAndHtml/index.css">
-    <script type="module" src="../javascript/new_password.js"></script>
+    <script src="../javascript/password_validation.js"></script>
     <title>camagru : passwor reset</title>
   </head>
 
@@ -16,12 +16,16 @@
       <div class="form" id="form">
         <form class="login-form" id="forum" name="forum">
           <h1>New password</h1>
-          <input type="password" id="password" name="password"
-            placeholder="password" required>
-          <!-- <div id="email-error-message" class="error-message"></div> -->
+          <input type="password" id="password" name="password" placeholder="password" required>
+          <div class="indicator">
+              <span class="weak"></span>
+              <span class="medium"></span>
+              <span class="strong"></span>
+            </div>
+            <div class="text"></div>
           <input type="password" id="confirm_password" name="confirm_password"
             placeholder="confirm password" required>
-          <!-- <div id="email-error-message" class="error-message"></div> -->
+          <div id="email-error-message" class="error-message"></div>
           <button type="submit">Reset the password</button>
         </form>
       </div>
